192516058d03a1b3d407e1299c9ff6a38d76e2b1
Marina <95050756+Marindala@users.noreply.github.com>
2023-08-29 17:03:53 -0300

    instance S3 client

--- server/S3.js
+++ server/S3.js
@@ -1,7 +1,10 @@
 require ('dotenv').config()
-require ('aws-sdk/client/')
+const {S3Client} = require ('aws-sdk/client-s3')
+
 
 const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
 const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION
 const AWS_PUBLIC_KEY = process.env.AWS_PUBLIC_KEY
 const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY
+
+new S3Client({region:AWS_BUCKET_REGION})
